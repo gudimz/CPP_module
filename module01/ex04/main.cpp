2a34d53012a41001e668e771f50f06cfeba47a78
agigi <agigi@student.21-school.ru>
2021-07-21 17:13:36 +0300

    ex04 maybe done

---
+++ module01/ex04/main.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv) {
+	if (argc != 4) {
+		std::cout << "Error!" << std::endl << "Wrong number of arguments" << std::endl;
+		return 1;
+	}
+	if (!argv[2] || !argv[3]) {
+		std::cout << "Error!" << std::endl << "You passed empty s1 or s2"  << std::endl;
+		return 1;
+	}
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	std::ifstream file(filename);
+	if (!file) {
+		std::cout << "Error!" << std::endl << "Could not open file" << std::endl;
+		return 1;
+	}
+	std::ofstream fileReplace(filename + ".replace");
+	std::string content;
+	int pos;
+	while (getline(file, content)) {
+		pos = content.find(s1);
+		if (pos != -1) {
+			content.erase(pos, s1.length());
+			content.insert(pos, s2);
+			fileReplace << content << std::endl;
+		} else {
+			fileReplace << content << std::endl;
+		}
+	}
+	file.close();
+	return 0;
+}
