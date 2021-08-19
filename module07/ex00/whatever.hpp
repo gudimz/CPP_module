d64f995cbd7f623a990bea717768245120d70170
GuDimz <zgudimz@gmail.com>
2021-08-20 00:23:37 +0300

    ex00 done

---
+++ module07/ex00/whatever.hpp
@@ -0,0 +1,30 @@
+# ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+# include <iostream>
+
+template <typename T>
+void swap(T& a, T& b) {
+	T tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T& min(T &a, T &b) {
+	if (a == b) {
+		return b;
+	}
+	return a < b ? a : b;
+}
+
+template <typename T>
+T& max(T &a, T &b) {
+	if (a == b) {
+		return b;
+	}
+	return a > b ? a : b;
+}
+
+#endif
